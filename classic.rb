f074d4419b619cddc8ea098ee83cc199b921d386
Philip McMahon <philip@cabin.local>
2014-12-08 12:04:12 -0700

    Initial

---
+++ classic.rb
@@ -0,0 +1,13 @@
+require 'mongo_mapper'
+
+class Classic
+  include MongoMapper::Document
+
+  key :dateTime,Time 
+  key :ampHours, String
+  key :dispavgVbatt, String
+  key :dispavgVpv , String
+  key :ibattDisplayS , String
+  key :watts, String
+  key :registers, Array
+end
