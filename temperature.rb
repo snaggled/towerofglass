97ed01c9939dd5dfdc44fa42a223f1f9c9fe5735
snaggled <philip@packetnode.com>
2014-12-09 20:01:26 -0700

    More refactor

--- temperature.rb
+++ temperature.rb
@@ -8,18 +8,8 @@ require 'net/http'
 MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 MongoMapper.database = "raspberry"
 
-def get(location)
-    url = "http://192.168.1.24/cgi-bin/%s.cgi" % location
-    u = URI.parse(url)
-    req = Net::HTTP::Get.new(u.to_s)
-    res = Net::HTTP.start(u.host, u.port) do |http|
-        http.request(req)
-    end
-    return Temperature.new(:c => /\st=(\-?\d+)/.match(res.body)[1].to_f / 1000)
-end
-
 r = Raspberry.new
 r.dateTime = DateTime.now
-r.inside = get('inside').in_fahrenheit
-r.outside = get('outside').in_fahrenheit
+r.inside = Temperature.get('inside').in_fahrenheit
+r.outside = Temperature.get('outside').in_fahrenheit
 r.save
